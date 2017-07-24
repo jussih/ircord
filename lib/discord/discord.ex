4fe6a5c5da625d869e85f79d0548090b2357326d
Jussi Heikkilä <jussih@gmail.com>
2017-07-24 18:42:42 +0300

    replace mention ids with usernames

--- lib/discord/discord.ex
+++ lib/discord/discord.ex
@@ -11,17 +11,8 @@ defmodule Ircord.DiscordBot do
 
   # Message Handler
   def handle_event({:message_create, payload}, state) do
-    spawn fn ->
-      if payload.data["author"]["id"] != state[:client_id] do
-        """
-        TODO: payload with mention, use this to decode mention id to name
-        %{data: %{"attachments" => [], "author" => %{"avatar" => "5dac354610b688fc5d7fe991b172f2b7", "discriminator" => "7512", "id" => 308968137678651393, "username" => "juba"}, "channel_id" => 321318083182460928, "content" => "<@321314936716525581>  wat", "edited_timestamp" => nil, "embeds" => [], "id" => 338301378277408770, "mention_everyone" => false, "mention_roles" => [], "mentions" => [%{"avatar" => nil, "bot" => true, "discriminator" => "5366", "id" => 321314936716525581, "username" => "kk-bot"}], "nonce" => "338301378130608128", "pinned" => false, "timestamp" => "2017-07-22T12:48:53.917000+00:00", "tts" => false, "type" => 0}, event_name: :MESSAGE_CREATE, op: :dispatch, seq_num: 11}
-        """
-        GenServer.call(:bridge, {:discord_message_received, _message_parser(payload)})
-        if MessageHelper.actionable_message_for_me?(payload, state) do
-          _command_parser(payload, state)
-        end
-      end
+    if payload.data["author"]["id"] != state[:client_id] do
+      GenServer.call(:bridge, {:discord_message_received, _message_parser(payload)})
     end
     {:ok, state}
   end
@@ -35,27 +26,8 @@ defmodule Ircord.DiscordBot do
   defp _message_parser(payload) do
     author_name = payload.data["author"]["username"]
     content  = payload.data["content"]
-    "<#{author_name}> #{content}"
-  end
-
-  # Select command to execute based off message payload
-  defp _command_parser(payload, state) do
-    case MessageHelper.msg_command_parse(payload) do
-      {nil, msg} ->
-        Logger.info("do nothing for message #{msg}")
-      {cmd, msg} ->
-        _execute_command({cmd, msg}, payload, state)
-    end
-  end
-
-  # Echo response back to user or channel
-  defp _execute_command({"example:echo", message}, payload, state) do
-    msg = String.upcase(message)
-    Channel.send_message(state[:rest_client], payload.data["channel_id"], %{content: "#{msg} yourself!"})
-  end
-
-  # Pong response to ping
-  defp _execute_command({"example:ping", _message}, payload, state) do
-    Channel.send_message(state[:rest_client], payload.data["channel_id"], %{content: "Pong!"})
+    mentions = payload.data["mentions"]
+    expanded_content = Enum.reduce(mentions, content, fn(mention, msg) -> String.replace(msg, "<@" <> Integer.to_string(mention["id"]) <> ">", "@" <> mention["username"]) end)
+    "<#{author_name}> #{expanded_content}"
   end
 end
