4b5ea5aa333e22bf2edd8af8387deef5c33f7327
Jussi Heikkilä <jussih@gmail.com>
2017-10-10 20:46:23 +0300

    optimize irc message chunking

--- lib/irc/messages.ex
+++ lib/irc/messages.ex
@@ -5,14 +5,13 @@ defmodule Ircord.IRC.Messages do
   def from_string(sender, string) do
     string
     |> String.split("\n", trim: true)
-    |> Enum.map(fn msg -> chunk_msg(msg, 410) end)  # RFC 1459: max length 510 + CRLF
-    |> List.flatten()
+    |> Enum.flat_map(fn msg -> chunk_msg(msg, 410) end)  # RFC 1459: max length 510 + CRLF
     |> Enum.map(fn msg -> "<#{sender}> #{msg}" end)
   end
 
   defp chunk_msg("", _size), do: []
   defp chunk_msg(string, size) do
     {chunk, tail} = String.split_at(string, size)
-    [chunk, chunk_msg(tail, size)]
+    [chunk | chunk_msg(tail, size)]
   end
 end
